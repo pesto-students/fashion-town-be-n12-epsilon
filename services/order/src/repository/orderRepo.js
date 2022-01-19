48247d1e75b1395434c3569ec771ccc02f07174e
vikram kumar <vikram.1rn14ec170@gmail.com>
2022-01-19 15:47:20 +0530

    remove console.log

--- services/order/src/repository/orderRepo.js
+++ services/order/src/repository/orderRepo.js
@@ -2,14 +2,11 @@ import { OrderModel } from '../models/order';
 
 export const addOrder = async (orderData)=>{
     const newOrder = new OrderModel({...orderData})
-    console.log(newOrder)
     const savedOrder = await newOrder.save();
     return savedOrder
 }
 
 export const getOrderListByEmailId = async (orderByEmailId) =>{
-    console.log(orderByEmailId)
     const orderList = await OrderModel.find({ orderByEmailId });
-    console.log(orderList)
     return orderList
 }
