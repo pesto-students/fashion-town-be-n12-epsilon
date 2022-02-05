e09587e635e6148397ed55a2d058fc0fcb548c6a
vikram kumar <vikram.1rn14ec170@gmail.com>
2022-02-05 22:10:39 +0530

    Move db credientials to .env file

--- services/products/config.js
+++ services/products/config.js
@@ -1,6 +1,7 @@
-const config ={
-    uri : "mongodb+srv://admin:admin123@cluster0.a9zwe.mongodb.net/fashion_town",
-    dbName: "fashion_town",
-    collectionName: "products"
-}
+import "dotenv/config"
+const config = {
+  dbUri: `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@cluster0.a9zwe.mongodb.net`,
+  dbName: "fashion_town",
+  collectionName: "products",
+};
 export default config
