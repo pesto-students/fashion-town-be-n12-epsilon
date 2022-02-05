1ac06064ce5f994102e03c821f4f5f2d0093dcb0
vikram kumar <vikram.1rn14ec170@gmail.com>
2022-02-05 22:58:28 +0530

    Add search and applied filters logs

--- services/products/config.js
+++ services/products/config.js
@@ -3,5 +3,7 @@ const config = {
   dbUri: `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@cluster0.a9zwe.mongodb.net`,
   dbName: "fashion_town",
   collectionName: "products",
+  appliedFilterLogFilePath: "/tmp/appliedFilterLogs.txt",
+  searchTextLogFilePath: "/tmp/textSearchLogs.txt",
 };
 export default config
--- services/products/resolvers/productBySearchInput.js
+++ services/products/resolvers/productBySearchInput.js
@@ -1,5 +1,6 @@
 import { SearchData } from "../databaseUtils/dbOperations";
-
+import * as fs from "fs";
+import config from "../config";
 const getPipeline = (searchInput) => {
   console.log(searchInput);
   return [
@@ -16,9 +17,15 @@ const getPipeline = (searchInput) => {
     },
   ];
 };
-
-export async function productBySearchInput(searchInput) {
-  const pipeline = getPipeline(searchInput.searchInput);
+const logSearchText = (searchText) => {
+  const searchTextString = JSON.stringify(searchText) + "\n";
+  fs.appendFile(config.searchTextLogFilePath, searchTextString, function (err) {
+    if (err) throw err;
+  });
+};
+export async function productBySearchInput({ searchInput }) {
+  const pipeline = getPipeline(searchInput);
+  logSearchText(searchInput);
   console.log(pipeline);
   try {
     const data = await SearchData(pipeline);
